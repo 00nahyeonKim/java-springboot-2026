5211edf1b62e91169d09545c404d267b66ccd6f2
00nahyeonKim <nahyeon000918@gmail.com>
2026-04-15 17:33:23 +0900

    4/15 커밋

---
+++ day08/studygroup/src/main/java/com/pknu26/studygroup/controller/BoardController.java
@@ -0,0 +1,95 @@
+package com.pknu26.studygroup.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.pknu26.studygroup.dto.Board;
+import com.pknu26.studygroup.service.BoardService;
+import com.pknu26.studygroup.validation.BoardForm;
+
+import jakarta.validation.Valid;
+
+//  Controller에서 중요한 것은 mapping url. 메서드명 중요하지 않음.
+@Controller
+@RequestMapping("/board")
+public class BoardController {
+
+    @Autowired
+    private BoardService boardService;
+
+    // 목록
+    @GetMapping("/list")
+    public String list(Model model) {
+        model.addAttribute("boardList", this.boardService.readBoardList());
+        return "/board/list";   // board 폴더 밑에 위치한 list.html을 리턴하라.
+    }
+
+    // 목록상세보기
+    @GetMapping("/detail/{boardId}")
+    public String detail(@PathVariable("boardId") Long boardId, Model model) {
+        Board board = this.boardService.readBoardById(boardId);
+        model.addAttribute("board", board);
+        return "/board/detail";
+    }
+
+    // 글쓰기 GET
+    @GetMapping("/create")
+    public String showCreateForm(BoardForm boardForm) {
+        return "/board/form";
+    }
+
+    // 글쓰기 POST
+    @PostMapping("/create")
+    public String create(@Valid BoardForm boardForm, BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) {
+            return "/board/form";
+        } 
+
+        this.boardService.createBoard(boardForm);
+        return "redirect:/board/list";
+    }
+    
+    // 글수정 GET
+    @GetMapping("/edit/{boardId}")
+    public String showEditForm(@PathVariable("boardId") Long boardId, Model model) {
+        Board board = boardService.readBoardById(boardId);
+
+        BoardForm boardForm = new BoardForm();
+        boardForm.setBoardId(board.getBoardId());
+        boardForm.setTitle(board.getTitle());
+        boardForm.setContent(board.getContent());
+        boardForm.setWriter(board.getWriter());
+
+        model.addAttribute("boardForm", boardForm);
+        return "/board/form";
+    }
+
+    // 글수정 POST
+    @PostMapping("/edit/{boardId}")
+    public String edit(@PathVariable("boardId") Long boardId,
+                       @Valid @ModelAttribute("boardForm") BoardForm boardForm,
+                       BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) {
+            return "/board/form";
+        }
+
+        boardForm.setBoardId(boardId);
+        this.boardService.updateBoard(boardForm);
+        return "redirect:/board/detail/" + boardId; // 수정된 자신글 상세로 이동
+    }
+
+    // 글삭제 POST
+    @PostMapping("/delete/{boardId}")
+    public String delete(@PathVariable("boardId") Long boardId) {
+        this.boardService.deleteBoard(boardId);
+        return "redirect:/board/list";
+    }
+
+}
